c05f0000180f4b0184b924316d275d3747c45bbb
Areeba Rao <areebarao108@gmail.com>
2026-07-22 23:16:32 +0530

    update test load model

--- scripts/test_load_model.py
+++ scripts/test_load_model.py
@@ -1,29 +1,20 @@
+import mlflow
 import mlflow.pyfunc
 import pytest
-from mlflow.tracking import MlflowClient
 
-# Set your remote tracking URI
 mlflow.set_tracking_uri("http://3.111.55.134:5000")
 
-@pytest.mark.parametrize("model_name, stage", [
-    ("yt_chrome_plugin_model", "staging"),])
-def test_load_latest_staging_model(model_name, stage):
-    client = MlflowClient()
-    
-    # Get the latest version in the specified stage
-    latest_version_info = client.get_latest_versions(model_name, stages=[stage])
-    latest_version = latest_version_info[0].version if latest_version_info else None
-    
-    assert latest_version is not None, f"No model found in the '{stage}' stage for '{model_name}'"
+@pytest.mark.parametrize("model_name, alias", [
+    ("yt_chrome_plugin_model", "staging"),
+])
+def test_load_latest_model(model_name, alias):
+
+    model_uri = f"models:/{model_name}@{alias}"
 
     try:
-        # Load the latest version of the model
-        model_uri = f"models:/{model_name}/{latest_version}"
         model = mlflow.pyfunc.load_model(model_uri)
-
-        # Ensure the model loads successfully
-        assert model is not None, "Model failed to load"
-        print(f"Model '{model_name}' version {latest_version} loaded successfully from '{stage}' stage.")
+        assert model is not None
+        print(f"{model_name}@{alias} loaded successfully")
 
     except Exception as e:
-        pytest.fail(f"Model loading failed with error: {e}")
+        pytest.fail(str(e))
